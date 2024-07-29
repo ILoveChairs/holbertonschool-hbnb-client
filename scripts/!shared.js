1ae2a11c654f0de2479a14f0900ddc38283bb22d
Matias Davezac <8675@holbertonstudents.com>
2024-07-29 13:41:42 -0300

    Added and linked scripts

---
+++ scripts/!shared.js
@@ -0,0 +1,16 @@
+
+/* This file can only contain functions and global variables,
+the reason is to avoid method override on document.
+*/
+
+function headerOnLoad() {
+  console.log('Header loaded!')
+}
+
+function bodyOnLoad() {
+  console.log('Body loaded!')
+}
+
+function footerOnLoad() {
+  console.log('Footer loaded!')
+}
---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+
+window.onload = () => {
+  headerOnLoad();
+  bodyOnLoad();
+  footerOnLoad();
+};
