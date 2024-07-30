1d2ae5a698373b8db773c242ed7d2eb1d6192d14
Matias Davezac <8675@holbertonstudents.com>
2024-07-29 23:33:44 -0300

    Stashing changes Revised and added js functions, fixed some css, fixed some stuff, also copied the api.

--- scripts/!shared.js
+++ scripts/!shared.js
@@ -1,16 +1,53 @@
 
-/* This file can only contain functions and global variables,
-the reason is to avoid method override on document.
+/* This file contain shared functions.
+'loadSharedFunctions' should be called on windows.load to get
+access to elements.
 */
 
-function headerOnLoad() {
-  console.log('Header loaded!')
+/* Functions from https://www.w3schools.com/js/js_cookies.asp */
+/* Set cookie modified to have fixed expire date, half a day*/
+function setCookie(cname, cvalue) {
+  const d = new Date();
+  d.setTime(d.getTime() + (12 * 60 * 60 * 1000));
+  let expires = 'expires='+d.toUTCString();
+  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
 }
 
-function bodyOnLoad() {
-  console.log('Body loaded!')
+function getCookie(cname) {
+  let name = cname + '=';
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
 }
 
-function footerOnLoad() {
-  console.log('Footer loaded!')
+/* Session token handlers, renames */
+function setJwtToken(token) {
+  setCookie('jwt', token);
+}
+
+function getJwtToken() {
+  return getCookie('jwt');
+}
+
+function checkJwtToken() {
+  if (getJwtToken() === '') {
+    return false;
+  }
+  return true;
+}
+
+/* To be called on every window.load */
+function loadSharedFunctions() {
+  loginButton = document.getElementById('login-button');
+  if (checkJwtToken()) {
+    loginButton.style = 'visibility: hidden';
+  }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,24 @@
 
+async function fetchPlaces(token) {
+  
+}
+
+function displayPlaces(places) {
+
+}
+
+function reloadPlaces() {
+  fetch('https://hellosalut.stefanbohacek.dev/')
+    .then((response) => response.json())
+    .then((data) => {displayPlaces()})
+    .catch((err) => {console.log(err.toString())});
+}
+
 window.onload = () => {
-  headerOnLoad();
-  bodyOnLoad();
-  footerOnLoad();
+  loadSharedFunctions()
+
+  countryFilter = document.getElementById('country-filter');
+  countryFilter.addEventListener('change', () => {reloadPlaces()});
+  reloadPlaces();
+
 };
